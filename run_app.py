c31f5df5bf8994f71d9e2052c35bd55531769807
Dany Rashwan <dannirash@outlook.com>
2023-09-28 23:07:30 -0400

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -2,13 +2,7 @@ import subprocess
 import platform
 
 def open_terminal(command):
-    system = platform.system()
-    if system == 'Darwin':  # macOS
-        subprocess.call(['osascript', '-e', f'tell application "Terminal" to do script "{command}"'])
-    elif system == 'Windows':  # Windows
-        subprocess.Popen(['start', 'cmd', '/k', command], shell=True)
-    elif system == 'Linux':  # Linux
-        subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', command])
+    subprocess.Popen(['start', 'cmd', '/k', command], shell=True)
 
 # Command to run Flask in the "backend" directory
 flask_command = 'cd backend && python -m flask --app app run'
