1a8767ed5ab913566f58491d4a79895fe7e5c743
Dany Rashwan <dannirash@outlook.com>
2023-09-29 01:28:41 -0400

    Saving frames under public directory
    next: need to read frames into website and load mood to sort music

--- src/components/CameraFeed.js
+++ src/components/CameraFeed.js
@@ -3,16 +3,13 @@ import "../App.css";
 import { CameraButton } from "./CameraButton";
 import "./CameraFeed.css";
 
-function CameraFeed() {
+ function CameraFeed() {
   return (
     <div className="camera-container">
       <h1>Take a picture</h1>
       <p>To scan your mood</p>
-      <div className="camera-btns">
-        <CameraButton />
-      </div>
+      <CameraButton />
     </div>
   );
 }
-
 export default CameraFeed;
