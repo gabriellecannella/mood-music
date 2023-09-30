7d801144f8ce9a9acd2609db4e4b264697708c67
gabriellecannella <73612182+gabriellecannella@users.noreply.github.com>
2023-09-30 14:42:59 -0400

    changes

--- src/components/BasicTextFields.js
+++ src/components/BasicTextFields.js
@@ -1,29 +1,29 @@
 // import * as React from 'react';
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
-import axios from 'axios';
-
+import axios from "axios";
 
 export default function BasicTextFields({ setRows }) {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
   const handleKeyPress = async (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       e.preventDefault();
 
-      axios.get(`http://127.0.0.1:5000/songs?arg1=${inputValue}`)
+      axios
+        .get(`http://127.0.0.1:5000/songs?arg1=${inputValue}`)
         .then((response) => {
           const data = response.data;
-          
+
           setRows(data);
-        })
+        });
       // console.log("choosing song")
 
       // await ChooseSongs(inputValue);
@@ -34,23 +34,24 @@ export default function BasicTextFields({ setRows }) {
 
   return (
     <div>
-      <h2>mood:</h2>
-    <Box
-      component="form"
-      sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-
-    >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" 
-              value={inputValue}  
-              onChange={handleInputChange}
-  
-        onKeyDown={handleKeyPress}
-/>
-    </Box>
+      <h1>Type how youre feeling</h1>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          id="outlined-basic"
+          label="Sad,Happy,Angry.."
+          variant="outlined"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyPress}
+        />
+      </Box>
     </div>
   );
 }
