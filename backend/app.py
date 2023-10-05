06bc78d9dd9c608a3bdaf34cc2bf0b751559358b
Dany Rashwan <dannirash@outlook.com>
2023-10-05 18:08:39 -0400

    cleaning

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,7 @@
-from flask import Flask, Response, request, send_file
-#from camera import VideoCamera
+from flask import Flask, Response, request
 from image_processing import process_image
 import pandas as pd
 from flask_cors import CORS, cross_origin
-from markupsafe import escape
-import tkinter as tk
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -53,27 +50,3 @@ def process_image_endpoint():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-'''    
-# Route to access the camera feed
-@app.route('/camera', methods=['GET'])
-def camera_feed():
-    # Create an instance of VideoCamera
-    root = tk.Tk()
-    video_camera = VideoCamera(root)
-    root.mainloop()
-    
-    def generate():
-        video_camera.start()  # Start the camera when the feed is requested
-        try:
-            while True:
-                frame = video_camera.update()
-                if frame is not None:
-                    yield (b'--frame\r\n'
-                           b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        except GeneratorExit:
-            # Clean up when the generator is closed (e.g., client disconnects)
-            video_camera.stop()
-
-    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
-'''
