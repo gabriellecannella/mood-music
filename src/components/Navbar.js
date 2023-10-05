996c68c4ce5e2652de01a748e72ca6e61914f7ea
Eyal weisman <120413260+eyal12121@users.noreply.github.com>
2023-10-06 00:27:53 +0300

    upload utton fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { Button } from "./Button";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -6,8 +6,12 @@ import { FileUpload, LibraryMusic, UploadFile } from "@mui/icons-material";
 import { CameraButton } from './CameraButton';
 import PopupModal from "./PopupModal";
 import MyPopup from "./MyPopup";
+import { useDropzone } from 'react-dropzone';
 
 function Navbar({setMood}) {
+
+
+
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
 
@@ -25,6 +29,59 @@ function Navbar({setMood}) {
   useEffect(() => {
     showButton();
   }, []);
+  const onDrop = useCallback( async (acceptedFiles) => {
+    // Handle the accepted files here (e.g., upload to a server)
+    console.log('Accepted files:', acceptedFiles[0]);
+    try {
+      const formData = new FormData();
+      formData.append('snapshot', acceptedFiles[0]);
+      const response =  fetch('http://127.0.0.1:5000/camera', {
+        method: 'POST',
+        body: formData,
+      }).then((response) =>{ 
+
+        if (response.ok) {
+          const label = response.text().then((label) => {
+            console.log({label});
+            setMood({label});
+
+          }
+            )
+
+        // const imageBlob = await response.blob();
+        // setAnnotatedSnapshot(URL.createObjectURL(imageBlob));
+      } else {
+        console.error('Error processing snapshot on the server');
+      }
+    })
+    } catch (error) {
+      console.error('Error sending snapshot to the server:', error);
+    }
+
+    
+    
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: '.jpg, .jpeg, .png'
+  });
+
+
+
+  function dataURItoBlob(dataURI) {
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+  
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+  
+    return new Blob([ab], { type: mimeString });
+  }
+
 
   const [isModalOpen, setModalOpen] = useState(false);
 
@@ -49,7 +106,8 @@ function Navbar({setMood}) {
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           </Link>
 
-          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+          <Link  className="navbar-links" {...getRootProps()}>
+          <input {...getInputProps()} />
             <UploadFile color="white" fontSize="large" />
           </Link>
 
