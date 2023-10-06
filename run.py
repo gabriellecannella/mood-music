b860e0158bf2a0a41517dace797260101138aad2
Dany Rashwan <dannirash@outlook.com>
2023-10-05 22:52:52 -0400

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,7 @@ import subprocess
 import os
 import platform
 
-def open_terminal(command, directory):
-    system = platform.system().lower()
-    
+def open_terminal(command, directory):   
     if system == 'darwin':
         subprocess.Popen(['osascript', '-e', f'tell application "Terminal" to do script "cd {directory} && {command}"'])
     elif system == 'windows':
@@ -14,11 +12,20 @@ def open_terminal(command, directory):
     else:
         print("Unsupported operating system")
 
+# OS type
+system = platform.system().lower()
+
 # Get the current working directory
 current_directory = os.getcwd()
 
+# Python version
+if system == 'darwin':
+    python = "python3"
+else:
+    python = "python"
+
 # Command to run Flask in the current directory
-flask_command = 'cd backend && python3 app.py'
+flask_command = 'cd backend && ' + python + ' app.py'
 
 # Command to run npm start in the current directory
 npm_command = 'npm start'
