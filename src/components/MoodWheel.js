71c6fa2832343875d51466272645b2d093a5734b
JacobLevine04 <108999054+JacobLevine04@users.noreply.github.com>
2023-10-14 11:09:42 -0400

    Second mood wheel implementation

--- src/components/MoodWheel.js
+++ src/components/MoodWheel.js
@@ -4,7 +4,6 @@ import './MoodWheel.css'; // Import your CSS file
 const MoodWheel = () => {
   const [selectedOuterMood, setSelectedOuterMood] = useState(null);
   const [selectedInnerMood, setSelectedInnerMood] = useState(null);
-  const [selectedMoodCombination, setSelectedMoodCombination] = useState('');
 
   const moodData = [
     {
@@ -15,12 +14,12 @@ const MoodWheel = () => {
     {
       outer: 'Sad',
       inner: ['Melancholic', 'Lonely', 'Despairing'],
-      color: '#6495ED', // Mood-specific text color (blue for Sad)
+      color: '#87CEEB', // Mood-specific text color (blue for Sad)
     },
     {
       outer: 'Excited',
       inner: ['Energetic', 'Thrilled', 'Nervous'],
-      color: '#FF4500', // Mood-specific text color (orange-red for Excited)
+      color: '#FFA500', // Orange for Excited (adjust as needed)
     },
     {
       outer: 'Calm',
@@ -38,63 +37,63 @@ const MoodWheel = () => {
   const handleOuterMoodSelect = (outerEmotion) => {
     setSelectedOuterMood(outerEmotion);
     setSelectedInnerMood(null); // Reset the inner mood when the outer mood changes
-    updateSelectedMoodCombination(outerEmotion, null);
   };
 
   const handleInnerMoodSelect = (innerEmotion) => {
     setSelectedInnerMood(innerEmotion);
-    updateSelectedMoodCombination(selectedOuterMood, innerEmotion);
-  };
-
-  const updateSelectedMoodCombination = (outer, inner) => {
-    if (outer && inner) {
-      setSelectedMoodCombination(`${outer}:${inner}`);
-    } else if (outer) {
-      setSelectedMoodCombination(outer);
-    } else {
-      setSelectedMoodCombination('');
-    }
   };
 
   return (
-    <div>
-      <div className="mood-wheel">
-        <div className="outer-ring">
-          {moodData.map((data, index) => (
-            <div
-              key={index}
-              className={`outer-emotion ${
-                selectedOuterMood === data.outer ? 'selected' : ''
-              }`}
-              onClick={() => handleOuterMoodSelect(data.outer)}
-              style={{ backgroundColor: data.color }}
-            >
-              {data.outer}
-            </div>
-          ))}
-        </div>
-        <div className="inner-round">
-          {selectedOuterMood &&
-            moodData
-              .find((data) => data.outer === selectedOuterMood)
-              .inner.map((innerEmotion, index) => (
-                <div
-                  key={index}
-                  className={`inner-emotion ${
-                    selectedInnerMood === innerEmotion ? 'selected' : ''
-                  }`}
-                  onClick={() => handleInnerMoodSelect(innerEmotion)}
-                  style={{ backgroundColor: moodData.find((data) => data.outer === selectedOuterMood).color }}
-                >
-                  {innerEmotion}
-                </div>
-              ))}
-        </div>
+    <div className="mood-container">
+      <div className="mood-circle">
+        {moodData.map((data, index) => (
+          <div
+            key={index}
+            className={`mood outer-mood ${
+              selectedOuterMood === data.outer ? 'selected' : ''
+            }`}
+            onClick={() => handleOuterMoodSelect(data.outer)}
+            style={{
+              backgroundColor: data.color,
+              transform: `rotate(${(index * 360) / moodData.length}deg) translateX(180px)`,
+            }}
+          >
+            {data.outer}
+          </div>
+        ))}
+        {selectedOuterMood &&
+          moodData
+            .find((data) => data.outer === selectedOuterMood)
+            .inner.map((innerEmotion, index) => (
+              <div
+                key={index}
+                className={`mood inner-mood ${
+                  selectedInnerMood === innerEmotion ? 'selected' : ''
+                }`}
+                onClick={() => handleInnerMoodSelect(innerEmotion)}
+                style={{
+                  backgroundColor: moodData.find(
+                    (data) => data.outer === selectedOuterMood
+                  ).color,
+                  transform: `rotate(${(index * (360 / 3)) + 30}deg) translateX(60px)`, // Adjust translateX value
+                }}
+              >
+                {innerEmotion}
+              </div>
+            ))}
       </div>
       <div className="selected-mood-text">
         <p>Selected Mood:</p>
-        <p style={{ color: moodData.find((data) => data.outer === selectedOuterMood)?.color }}>
-          {selectedMoodCombination || 'None'}
+        <p
+          style={{
+            color: moodData.find((data) => data.outer === selectedOuterMood)?.color,
+            fontSize: '24px', // Increase font size for the key
+            textAlign: 'center', // Center-align the text
+          }}
+        >
+          {selectedOuterMood && selectedInnerMood
+            ? `${selectedOuterMood}:${selectedInnerMood}`
+            : selectedOuterMood || 'None'}
         </p>
       </div>
     </div>
