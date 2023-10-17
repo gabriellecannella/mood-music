e3042ce4ed43e4daee22f0f1290e6041623eea85
Dany Rashwan <61055337+dannirash@users.noreply.github.com>
2023-10-17 12:48:39 -0400

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "../App.css";
-import { Button } from "./Button";
 import "./HeroSection.css";
-import { Button_Cam } from "./Button_Cam";
 import MoodWheel from "./MoodWheel";
 
 function HeroSection() {
