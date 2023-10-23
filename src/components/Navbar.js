e7a5287ce31b021b87920edfb0b3e7e1fbcc3762
Dany Rashwan <dannirash@outlook.com>
2023-10-23 15:04:26 -0400

    navbar edit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,104 +6,73 @@ import { CameraButton } from './CameraButton';
 import MyPopup from "./PopupModal";
 import { useDropzone } from 'react-dropzone';
 
-function Navbar({setMood}) {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
-  const closeMobileMenu = () => setClick(false);
+function Navbar({ setMood }) {
+  const [isModalOpen, setModalOpen] = useState(false);
 
-  const showButton = () => {
-    if (window.innerWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
+  const toggleModal = () => setModalOpen(!isModalOpen);
 
-  useEffect(() => {
-    showButton();
-  }, []);
-  const onDrop = useCallback( async (acceptedFiles) => {
-    // Handle the accepted files here (e.g., upload to a server)
-    console.log('Accepted files:', acceptedFiles[0]);
+  const onDrop = useCallback(async (acceptedFiles) => {
     try {
       const formData = new FormData();
       formData.append('snapshot', acceptedFiles[0]);
-      const response =  fetch('http://127.0.0.1:5000/camera', {
+
+      const response = await fetch('http://127.0.0.1:5000/camera', {
         method: 'POST',
         body: formData,
-      }).then((response) =>{ 
+      });
 
-        if (response.ok) {
-          const label = response.text().then((label) => {
-            console.log(label);
-            setMood(label);
-
-          }
-            )
-
-        // const imageBlob = await response.blob();
-        // setAnnotatedSnapshot(URL.createObjectURL(imageBlob));
+      if (response.ok) {
+        const label = await response.text();
+        setMood(label);
       } else {
         console.error('Error processing snapshot on the server');
       }
-    })
     } catch (error) {
       console.error('Error sending snapshot to the server:', error);
     }
-
-    
-    
-  }, []);
+  }, [setMood]);
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    accept: '.jpg, .jpeg, .png'
+    accept: '.jpg, .jpeg, .png',
   });
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 960) {
+        setModalOpen(false);
+      }
+    };
 
+    window.addEventListener("resize", handleResize);
 
-  function dataURItoBlob(dataURI) {
-    const byteString = atob(dataURI.split(',')[1]);
-    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-    const ab = new ArrayBuffer(byteString.length);
-    const ia = new Uint8Array(ab);
-  
-    for (let i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-    }
-  
-    return new Blob([ab], { type: mimeString });
-  }
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-
-  const [isModalOpen, setModalOpen] = useState(false);
-
-  window.addEventListener("resize", showButton);
   return (
     <>
-    <MyPopup showModal={ isModalOpen } setModal={ setModalOpen } setMood={setMood} />
+      <MyPopup showModal={isModalOpen} setModal={setModalOpen} setMood={setMood} />
 
       <nav className="navbar">
         <div className="navbar-container">
-          <Link className="nav-links" onClick={ () => setModalOpen(true) }>
+          <Link className="nav-links" onClick={toggleModal}>
             <CameraButton color="white" fontSize="large" />
           </Link>
-          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MOOD
-            MUSIC&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          </Link>
-
-          <Link  className="navbar-links" {...getRootProps()}>
-          <input {...getInputProps()} />
+          <Link to="/" className="navbar-logo">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            MOOD MUSIC
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </Link>
+          <Link className="nav-links" {...getRootProps()}>
+            <input {...getInputProps()} />
             <UploadFile color="white" fontSize="large" />
           </Link>
-
         </div>
       </nav>
     </>
