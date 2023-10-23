ab61a741b579288b81b46c133e3a47a2926c0716
Dany Rashwan <dannirash@outlook.com>
2023-10-23 14:41:28 -0400

    Update TextFields.js

--- src/components/TextFields.js
+++ src/components/TextFields.js
@@ -8,8 +8,8 @@ export default function TextFields({ mood, genre }) {
       <div className="text-fields-container">
         {genre ? (
           <>
-            <h2 className="centered-text">Mood is {mood}.</h2>
-            <h1 className="centered-text">Here are some {genre} music:</h1>
+            <h2 className="centered-text">Mood: {mood}</h2>
+            <h1 className="centered-text">Suggesting: {genre} music</h1>
           </>
         ) : null}
       </div>
