104c4c1600968e816ee27c9da0a3f795c48ea29d
Dany Rashwan <dannirash@outlook.com>
2023-10-23 12:55:16 -0400

    new bgd

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import CustomPieChart from "./PieChart";
 function HeroSection({setMood}) {
   return (
     <div className="hero-container">
-      <video src="/videos/Back-vid.mp4" autoPlay loop muted />
+      <video src="/videos/video-3.mp4" autoPlay loop muted />
       <div className="image-container">
         <CustomPieChart setMood = {setMood}/>
       </div>
