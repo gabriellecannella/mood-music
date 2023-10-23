79fe342f21904df8b8e4c02ba1321d1f2e755a4a
Dany Rashwan <dannirash@outlook.com>
2023-10-23 14:26:19 -0400

    Update TextFields.js

--- src/components/TextFields.js
+++ src/components/TextFields.js
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import "./TextFields.css";
 
-export default function TextFields({ setRows,  mood, genre }) {
+export default function TextFields({ mood, genre }) {
+  mood = mood.toLowerCase();
   if(genre != ""){
-  return (
-    <div>
-      <h2 className="centered-text" >
-        Your mood is: {mood}
-        </h2>
-      <h1 className="centered-text" >
-      Suggesting: {genre} music
-      </h1>
-    </div>
-  );
+    return (
+      <div className="text-fields-container">
+        {genre ? (
+          <>
+            <h2 className="centered-text">Mood is {mood}.</h2>
+            <h1 className="centered-text">Here are some {genre} music:</h1>
+          </>
+        ) : null}
+      </div>
+    );
   }
   else{
-    return(<></>);
+    return (
+      <div className="text-fields-container">
+        <h2 className="centered-text">How are you feeling?</h2>
+      </div>
+    );
   }
 }
