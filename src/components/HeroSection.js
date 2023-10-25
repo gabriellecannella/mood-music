6882348aa39b0945bcca7a268779e20c6d03c6fe
Dany Rashwan <dannirash@outlook.com>
2023-10-25 18:08:29 -0400

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import CustomPieChart from "./PieChart";
 function HeroSection({setMood}) {
   return (
     <div className="hero-container">
-      <video src="/videos/background.mp4" autoplay loop muted />
+      <video src="/videos/background.mp4" autoPlay loop muted />
       <div className="image-container">
         <CustomPieChart setMood = {setMood}/>
       </div>
