4d0deb291afcca9ba8e6bc9177346c869dba8858
Eyal weisman <120413260+eyal12121@users.noreply.github.com>
2023-11-15 20:39:19 +0200

    fixed camera

--- src/components/pages/Camera.js
+++ src/components/pages/Camera.js
@@ -1,17 +1,41 @@
 import React from "react";
 import "../../App.css";
 import PopupModal from '../PopupModal';
-
+import {Button} from '../Button'
+import '../CameraFeed.css'
+ 
 function Camera({isModalOpen, setModalOpen}) {
   const toggleModal = () => setModalOpen(!isModalOpen);
 
   return(
-    <div>
+    <div className="camera-container-two">
+        <h1>Take a picture</h1>
+       <p>To scan your mood</p>
 
-      <PopupModal />
-      <button onClick={toggleModal}>click here!</button>
+      {/* <PopupModal /> */}
+      <button onClick = {toggleModal}
+        className="picturebutton"
+      >Take a Picture</button>
     </div>
   ); 
 }
 
 export default Camera;
+
+
+// function Camera({isModalOpen, setModalOpen}) {
+//   const toggleModal = () => setModalOpen(!isModalOpen);
+
+//   return(
+//     <div className="camera-container">
+//       <button onClick={toggleModal}>here!</button>
+//       <h1>Take a picture</h1>
+//       <p>To scan your mood</p>
+//       <div className="camera-btns">
+
+//       </div>
+//     </div>
+//   ); 
+// }
+
+// export default Camera;
